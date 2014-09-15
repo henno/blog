c4b738e63540dd0759e877258736f4903fe448fb
Maile.Rohiväli <maile.rohivali@gmail.com>
2014-09-15 15:06:20 +0300

    Add login, logout option

--- templates/master_template.php
+++ templates/master_template.php
@@ -60,11 +60,9 @@
 					</ul>
 				</li>
 			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="../navbar/">Default</a></li>
-				<li><a href="../navbar-static-top/">Static top</a></li>
-				<li class="active"><a href="./">Fixed top</a></li>
-			</ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li ><a href="<?=BASE_URL?><?=$auth->logged_in == true? 'logout':'login'?>"><?=$auth->logged_in == true? 'Logout':'Login'?></a></li>
+            </ul>
 		</div><!--/.nav-collapse -->
 	</div>
 </div>

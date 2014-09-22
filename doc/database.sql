b27ff89a48d1fa74ab6529fac2107d19f4bfaf0f
Maile.Rohiväli <maile.rohivali@gmail.com>
2014-09-22 15:24:11 +0300

    update db dump

--- doc/database.sql
+++ doc/database.sql
@@ -3,19 +3,13 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Sept 10, 2014 kell 12:37 PL
+-- Loomise aeg: Sept 22, 2014 kell 02:23 PL
 -- Serveri versioon: 5.6.17
 -- PHP versioon: 5.5.12
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Andmebaas: `blog`
@@ -30,21 +24,26 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
   `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `comment_subject` varchar(50) NOT NULL,
   `comment_author` varchar(100) NOT NULL,
   `comment_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `comment_text` text NOT NULL,
   `post_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`comment_id`),
   KEY `post_id` (`post_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Andmete tõmmistamine tabelile `comment`
 --
 
-INSERT INTO `comment` (`comment_id`, `comment_subject`, `comment_author`, `comment_created`, `comment_text`, `post_id`) VALUES
-(3, 'Tere', 'Nublu', '2014-09-10 10:32:17', 'Tere', 3);
+INSERT INTO `comment` (`comment_id`, `comment_author`, `comment_created`, `comment_text`, `post_id`) VALUES
+(3, 'Nublu', '2014-09-10 10:32:17', 'Tere', 3),
+(4, 'Klaabu', '2014-09-10 13:00:24', 'teretere', 3),
+(5, 'Klaabu', '2014-09-10 13:00:31', 'halloo', 3),
+(6, 'Klaabu', '2014-09-10 13:01:24', 'halloo', 3),
+(7, 'Klaabu', '2014-09-15 11:44:49', 'klaabu', 3),
+(8, 'Klaabu', '2014-09-15 11:45:02', 'jhuuuhu', 3),
+(9, 'Klaabu', '2014-09-15 11:45:15', 'midaiganes', 3);
 
 -- --------------------------------------------------------
 
@@ -61,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `post` (
   `user_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`post_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Andmete tõmmistamine tabelile `post`
@@ -69,7 +68,18 @@ CREATE TABLE IF NOT EXISTS `post` (
 
 INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `user_id`) VALUES
 (3, 'Halloo', 'Hallo Dolly!', '2014-09-04 11:44:42', 1),
-(4, 'Tere', 'Tere maailm!', '2014-09-04 11:44:42', 1);
+(4, 'Tere', 'Tere maailm!', '2014-09-04 11:44:42', 1),
+(5, 'kuku', '', '2014-09-17 12:53:17', 1),
+(6, 'kuku', '', '2014-09-17 12:56:30', 1),
+(7, 'kuku', '', '2014-09-17 12:57:41', 1),
+(8, 'tere', '', '2014-09-17 12:57:49', 1),
+(9, 'Test', 'Test', '2014-09-17 13:33:01', 2),
+(10, '', '', '2014-09-17 14:32:29', 1),
+(11, '', '', '2014-09-17 14:32:35', 1),
+(12, '', '', '2014-09-17 14:45:15', 1),
+(13, 'TERE', 'MAAILM', '2014-09-17 15:17:20', 1),
+(14, 'tere veelkord', 'ma kirjutan jälle', '2014-09-17 15:17:35', 1),
+(15, 'hallo kosmos', 'testing, testing', '2014-09-22 07:59:15', 1);
 
 -- --------------------------------------------------------
 
@@ -91,7 +101,9 @@ CREATE TABLE IF NOT EXISTS `post_tags` (
 
 INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
 (3, 1),
-(4, 2);
+(5, 1),
+(4, 2),
+(6, 2);
 
 -- --------------------------------------------------------
 
@@ -125,16 +137,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UNIQUE` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Andmete tõmmistamine tabelile `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
-(1, 'demo', 'demo', 0);
+INSERT INTO `user` (`user_id`, `username`, `password`, `active`, `email`) VALUES
+(1, 'demo', 'demo', 0, ''),
+(2, 'klaabu', 'nipi', 0, 'klaabu@klaabu.ee'),
+(3, 'sipsik', 'sipsik', 0, ''),
+(4, 'nublu', 'nublu', 0, '');
 
 --
 -- Tõmmistatud tabelite piirangud
@@ -159,7 +176,3 @@ ALTER TABLE `post_tags`
   ADD CONSTRAINT `post_tags_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
   ADD CONSTRAINT `post_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`);
 SET FOREIGN_KEY_CHECKS=1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

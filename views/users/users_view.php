7ebff8d7f8ed9cb84e709caf37d3b30908bae664
Henno Täht <henno.taht@gmail.com>
2014-09-24 14:28:26 +0300

    Update user properties page

--- views/users/users_view.php
+++ views/users/users_view.php
@@ -1,6 +1,26 @@
-<ul class="list-group">
-    <? foreach ($user as $attribute => $value): ?>
-        <li class="list-group-item"><?=$attribute?>:<?=$value?></li>
-    <? endforeach ?>
-</ul>
+<h1>User '<?= $user['username'] ?>'</h1>
+<table class="table table-bordered">
+    <tr>
+        <th>Username</th>
+        <td><?= $user['username'] ?></td>
+    </tr>
+    <tr>
+        <th>Password</th>
+        <td><?= $user['password'] ?></td>
+    </tr>
+    <tr>
+        <th>Active</th>
+        <td><?= $user['active'] ?></td>
+    </tr>
+    <tr>
+        <th>Email</th>
+        <td><?= $user['email'] ?></td>
+    </tr>
+</table>
 
+<!-- EDIT BUTTON -->
+<form action="users/edit/<?= $user['user_id'] ?>">
+    <button class="btn btn-info">
+        Edit <?= $user['username'] ?>
+    </button>
+</form>

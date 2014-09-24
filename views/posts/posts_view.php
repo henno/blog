5dbf31a666f9a15261e2969d04125c58ab67969d
Henno Täht <henno.taht@gmail.com>
2014-09-24 15:41:05 +0300

    Beautify single post view

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,37 +1,35 @@
-<?foreach ($tags as $tag):?><a href="#"><span class="label label-info"><?=$tag['tag_name']?></span></a> <?endforeach?>
+<? foreach ($tags as $tag): ?><a href="#"><span class="label label-info"><?= $tag['tag_name'] ?></span>
+    </a> <? endforeach ?>
 <div class="background"></div>
 
-<div id="post" class="col-xs-offset-1 col-xs-11 col-sm-offset-5 col-sm-6 col-md-offset-3 col-md-5">
-    <article id="post-1001" class="active">
-        <h1 class="title">
-            <?=$post['post_subject']?>
-            <button type="button" class="close" aria-hidden="true">×</button>
-        </h1>
-        <div class="post">
-            <img class="post-img" src="http://d.gr-assets.com/books/1358275334l/2767052.jpg" alt="The Hunger Games">
+<article id="post-1001" class="active">
+    <h1 class="title">
+        <?= $post['post_subject'] ?>
+    </h1>
+    <div class="post">
 
-            <p>
-                <?=$post['post_text']?>
-            </p>
-        </div>
-    </article>
-</div>
-<? foreach ($comments as $comment): ?>
+        <p>
+            <?= $post['post_text'] ?>
+        </p>
+    </div>
+</article>
 <div class="commentBox">
     <ul class="commentList">
-        <li>
-            <div class="commenterImage">
-                <img src="http://lorempixel.com/50/50/people/6" />
-            </div>
-            <div class="commentText">
-                <?=$comment['comment_created']?>
-                <?=$comment['comment_author']?>
-                <p><?=$comment['comment_text']?></p>
-            </div>
-        </li>
+        <? foreach ($comments as $comment): ?>
+            <li>
+                <div class="commenterImage">
+                    <img src="http://lorempixel.com/50/50/people/6"/>
+                </div>
+                <div class="commentText">
+                    <?= $comment['comment_created'] ?>
+                    <?= $comment['comment_author'] ?>
+                    <p><?= $comment['comment_text'] ?></p>
+                </div>
+            </li>
+        <? endforeach ?>
     </ul>
     <form class="form-inline" method="post" role="form">
-                <div class="form-group">
+        <div class="form-group">
             <input class="form-control" type="text" placeholder="Your comments" name="data[comment_text]"/>
         </div>
         <div class="form-group">
@@ -40,4 +38,3 @@
     </form>
 </div>
 
-<? endforeach ?>

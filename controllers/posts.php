89b595f0f33a199d4a2eb7a1091b2a9692811935
Henno Täht <henno.taht@gmail.com>
2014-10-13 09:24:41 +0300

    Test commit

--- controllers/posts.php
+++ controllers/posts.php
@@ -9,7 +9,7 @@ class posts extends Controller{
         foreach ($_tags as $tag) {
             $this->tags[$tag['post_id']][] = $tag['tag_name'];
         }
-        echo "TESTTESTTEST";
+        echo "HOOHOO";
 	}
 
     function index_ajax(){
